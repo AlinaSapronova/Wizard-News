b89426e6df43214a5429e75ea1c8ccee2b7c137b
jessicachiarella <jessica.chiarella@gmail.com>
2022-07-13 15:07:42 -0500

    got through styling the initial route

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
 const express = require("express");
-const morgan = require("morgan")
-const postBank = require('./postBank')
+const morgan = require("morgan");
+const postBank = require("./postBank");
 const app = express();
+
+app.use(express.static("public"));
 
 app.use(morgan("dev"));
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
   const posts = postBank.list();
 
-  const html = `"<!DOCTYPE html>
+  const html = `<!DOCTYPE html>
   <html>
-  <head›
-  ‹title›Wizard News</title›
-  </head›
+  <head>
+  <title>Wizard News</title>
+  <link rel="stylesheet" href="/style.css" />
+  </head>
   <body>
-  <ul>
-  ${posts.map(post =>
-     `<li>${post.data.title}</li>
-      <li>${post.data.author}</li>`)}
-  </ul>
+  <div class="news-list">
+  <header><img src="/logo.png"/>Wizard News</header>
+  ${posts
+    .map(
+      (post) => `
+    <div class='news-item'>
+          <p>
+          <span class="news-position">${post.id}. ▲</span>${post.title}  
+          <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+    )
+    .join("")}
+    </div>
   </body>
-  </html>`;
-
+</html>`;
   res.send(html);
-  console.log(html)
-
 });
-
 
 const PORT = 1337;
 
