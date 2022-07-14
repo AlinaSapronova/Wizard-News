825eea8ddaedb0cd04ccb4d6bbb4f7609a117daa
jessicachiarella <jessica.chiarella@gmail.com>
2022-07-14 12:56:06 -0500

    trying the html bonus

--- app.js
+++ app.js
@@ -2,72 +2,26 @@ const express = require("express");
 const morgan = require("morgan");
 const postBank = require("./postBank");
 const app = express();
-const timeAgo = require('node-time-ago');
+const postList = require("./views/postList");
+const postDetails = require("./views/postDetails") ;
 
-app.use(express.static("public"));
+
+app.use(express.static(__dirname + "public"));
 
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const posts = postBank.list();
-  const html = `<!DOCTYPE html>
-  <html>
-  <head>
-  <title>Wizard News</title>
-  <link rel="stylesheet" href="/style.css" />
-  </head>
-  <body>
-  <div class="news-list">
-  <header><img src="/logo.png"/>Wizard News</header>
-  ${posts
-    .map(
-      (post) => `
-    <div class='news-item'>
-          <p>
-          <span class="news-position">
-          <a href="/posts/${post.id}">▲${post.title}</a> 
-          <small>(by ${post.name})</small>
-          </p>
-          <small class="news-info">
-            ${post.upvotes} upvotes | ${timeAgo(post.date)}
-          </small>
-        </div>`
-    )
-    .join("")}
-    </div>
-  </body>
-</html>`;
-  res.send(html);
+  
+  res.send(postList(posts));
 });
 
 app.get('/posts/:id', (req, res) => {
-  const id = req.params.id;
-  const post = postBank.find(id);
+  const post = postBank.find(req.params.id);
   if (!post.id) {
     next()
   }else{
-  res.send(`<!DOCTYPE html>
-  <html>
-  <head>
-  <title>Wizard News</title>
-  <link rel="stylesheet" href="/style.css" />
-  </head>
-  <body>
-  <div class="news-list">
-  <header><img src="/logo.png"/>Wizard News</header>
-  <div class='news-item'>
-  <p>
-  <span class="news-position"></span>${post.title} <small>(by ${post.name})</small>  
-   </p>
-   <p>${post.content}</p>
-  <small class="news-info">
-   ${post.date}
-  </small>
-</div>
-</div>
-</body>
-</html>`
-  );
+  res.send(postDetails (post));
   
 
 }})
---
+++ views/postDetails.js
@@ -0,0 +1,22 @@
+
+module.exports = (post) => `<!DOCTYPE html>
+<html>
+<head>
+<title>Wizard News</title>
+<link rel="stylesheet" href="/style.css" />
+</head>
+<body>
+<div class="news-list">
+<header><img src="/logo.png"/>Wizard News</header>
+<div class='news-item'>
+<p>
+<span class="news-position"></span>${post.title} <small>(by ${post.name})</small>  
+ </p>
+ <p>${post.content}</p>
+<small class="news-info">
+ ${post.date}
+</small>
+</div>
+</div>
+</body>
+</html>`
